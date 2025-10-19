bbbdc1723acfeff20ec97cb7a5567217f4b440df
Clint E <clinton.eads92@gmail.com>
2025-10-19 14:29:50 -0400

    Added run python function and system prompt config

---
+++ functions/run_python_file.py
@@ -0,0 +1,42 @@
+import subprocess, os
+
+def run_python_file(working_directory, file_path, args=[]):
+
+    # Ensure the file path is within the working directory
+    abs_working_dir = os.path.abspath(working_directory)
+    target_path = os.path.abspath(os.path.join(working_directory, file_path))
+    if not target_path.startswith(abs_working_dir):
+        return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+    
+    # Check if file path leads to a valid file
+    if not os.path.isfile(target_path):
+        return f'Error: File "{file_path}" not found.'
+
+    # Check if the file is a Python file
+    if not target_path.endswith(".py"):
+        return f'Error: "{file_path}" is not a Python file.'
+    
+    # Run the Python file with arguments and capture output
+    try:
+        commands = ["python", target_path]
+        if args:
+            commands.extend(args)
+        result = subprocess.run(
+            commands,
+            capture_output=True,
+            text=True,
+            timeout=30,
+            cwd=abs_working_dir,
+        )
+        output = []
+        if result.stdout:
+            output.append(f"STDOUT:\n{result.stdout}")
+        if result.stderr:
+            output.append(f"STDERR:\n{result.stderr}")
+
+        if result.returncode != 0:
+            output.append(f"Process exited with code {result.returncode}")
+
+        return "\n".join(output) if output else "No output produced."
+    except Exception as e:
+        return f"Error: executing Python file: {e}"      
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os, sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from config import SYSTEM_PROMPT
 
 def main():
 
@@ -16,7 +17,11 @@ def main():
     else:
         user_prompt = sys.argv[1]
     messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)]),]
-    response = client.models.generate_content(model = "gemini-2.0-flash-001", contents = messages)
+    response = client.models.generate_content(
+        model = "gemini-2.0-flash-001", 
+        contents = messages, 
+        config=types.GenerateContentConfig(system_instruction=SYSTEM_PROMPT),
+        )
 
     # Check for verbose flag in command line arguments. If present print user prompt and token counts.
     if "--verbose" in sys.argv:
